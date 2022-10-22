7b66fc17bcf975ed4a7d3495101465c091316201
u1189630@eng.utah.edu <u1189630@eng.utah.edu>
2022-10-22 15:13:45 -0600

    NavBar looks good

--- secscanner/src/App.js
+++ secscanner/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar/index'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/index';
+import About from './pages/about'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" exact componenet={Home} />
+        <Route path="/about" exact componenet={About} />
+      </Routes>
+    </Router>
   );
 }
 
--- secscanner/src/components/Navbar/index.js
+++ secscanner/src/components/Navbar/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './NavbarElements';
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <>
+      <Nav>
+        <NavLink to="/">
+          <h1>SEC Scanner</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+          <NavLink to="/signIn" activeStyle>
+            Sign In
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/signIn">Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
   )
 }
 
